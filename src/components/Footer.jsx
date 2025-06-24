88d8de31a9a67a46c6adc4167215f7876587148d
MartinaK <martinanemeckova8@gmail.com>
2025-06-24 11:16:15 +0200

    first part done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './footer.css'
+
+const Footer = () => {
+    return(
+        <footer>
+            <div className="container">
+                <div className="footer__content">
+                    Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
